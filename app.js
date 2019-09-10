1a76fbbdd81d33fd15938796a252b643e30956e8
fbzr <fabricio.spb@gmail.com>
2019-09-10 14:24:04 -0700

    deploy and allow sending contact form

--- app.js
+++ app.js
@@ -13,40 +13,41 @@ app.use(express.static("public"));
 app.get("/", (req,res) => res.render("home"));
 
 app.post("/contact", (req,res) => {
-    // let name = req.body.name;
-    // let email = req.body.email;
-    // let message = req.body.message;
+    let name = req.body.name;
+    let email = req.body.email;
+    let message = req.body.message;
   
-    // const output = '<p>You have a new message from FabricioBezerra.com</p><h3>Contact Details</h3><ul><li>Name: ' + name + '</li><li>Email: ' + email + '</li></ul><h3>Message:</h3><p>' + message + '</p>';
+    const output = '<p>You have a new message from FabricioBezerra.com</p><h3>Contact Details</h3><ul><li>Name: ' + name + '</li><li>Email: ' + email + '</li></ul><h3>Message:</h3><p>' + message + '</p>';
   
-    // let transporter = nodeMailer.createTransport({
-    //   host: 'smtp.office365.com', // Office 365 server
-    //   port: 587, // secure SMTP
-    //   secure: false, // false for TLS - as a boolean not string - but the default is false so just remove this completely
-    //   auth: {
-    //     user: process.env.EMAIL,
-    //     pass: process.env.EMAIL_PASS
-    //   },
-    //   tls: {
-    //     ciphers: 'SSLv3'
-    //   }
-    // });
+    let transporter = nodeMailer.createTransport({
+      host: 'smtp.office365.com', // Office 365 server
+      port: 587, // secure SMTP
+      secure: false, // false for TLS - as a boolean not string - but the default is false so just remove this completely
+      auth: {
+        user: process.env.EMAIL,
+        pass: process.env.EMAIL_PASS
+      },
+      tls: {
+        ciphers: 'SSLv3'
+      }
+    });
   
-    // let mailOptions = {
-    //   from: '"Contact - FabricioBezerra.com" <contact@fabriciobezerra.com>', // sender address
-    //   to: "fabricio.spb@gmail.com, contact@fabriciobezerra.com", // list of receivers
-    //   subject: "Message from FabricioBezerra.com", // Subject line
-    //   text: message, // plain text body
-    //   html: output // html body
-    // };
+    let mailOptions = {
+      from: '"Contact - FabricioBezerra.com" <contact@fabriciobezerra.com>', // sender address
+      to: "fabricio.spb@gmail.com, contact@fabriciobezerra.com", // list of receivers
+      subject: "Message from FabricioBezerra.com", // Subject line
+      text: message, // plain text body
+      html: output // html body
+    };
   
-    // transporter.sendMail(mailOptions, (error, info) => {
-    //   if (error) {
-    //     res.send("Error: " + error);
-    //   } else {
-    //     console.log('Message %s sent: %s', info.messageId, info.response);
-    //   }
-    // });
+    transporter.sendMail(mailOptions, (error, info) => {
+      if (error) {
+        res.send("Error: " + error);
+      } else {
+        console.log('Message %s sent: %s', info.messageId, info.response);
+      }
+    });
+    
     console.log("email sent");
 });
 
