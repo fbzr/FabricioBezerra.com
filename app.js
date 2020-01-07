e727e026f7fda0fd08141064ab25c584e43e422a
fbzr <fabricio.spb@gmail.com>
2020-01-07 12:56:50 -0800

    add google recaptcha v3, express-validator, axios, create submit form event handler

--- app.js
+++ app.js
@@ -3,22 +3,56 @@ const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const nodeMailer = require("nodemailer");
 require("dotenv").config();
+const axios = require('axios');
+const { check, validationResult } = require("express-validator");
 
 const app = express();
 
 app.set("view engine", "ejs");
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.json({ extended: false }));
 app.use(express.static("public"));
 
-app.get("/", (req,res) => res.render("home"));
 
-app.post("/contact", (req,res) => {
-    let name = req.body.name;
-    let email = req.body.email;
-    let message = req.body.message;
-  
+app.get("/", (req,res) => {
+  res.render("home")
+});
+
+app.post(
+  "/contact",
+  [
+    check('name', 'Name is required').notEmpty(),
+    check('email', 'Please include a valid email').isEmail(),
+    check('message', 'Please include your message').notEmpty()
+  ],
+  async (req,res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      //bad request
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const { name, email, message, token } = req.body;    
+
+    // Google Recaptcha
+    const recaptcha = await axios.post(
+      `https://www.google.com/recaptcha/api/siteverify?secret=${process.env.SECRET_KEY}&response=${token}`, 
+      {}, 
+      {
+        headers: {
+            "Content-Type": "application/x-www-form-urlencoded; charset=utf-8"
+        }
+      }
+    );
+    if (recaptcha.data.success === false || recaptcha.data.score < 0.5) {
+      return res.status(401).json({ 
+        errors: [{
+          msg: "Sorry, your message was detected as spam"
+        }] 
+      })
+    }
+
+    // Node Mailer
     const output = '<p>You have a new message from FabricioBezerra.com</p><h3>Contact Details</h3><ul><li>Name: ' + name + '</li><li>Email: ' + email + '</li></ul><h3>Message:</h3><p>' + message + '</p>';
-  
     let transporter = nodeMailer.createTransport({
       host: 'smtp.office365.com', // Office 365 server
       port: 587, // secure SMTP
@@ -40,15 +74,17 @@ app.post("/contact", (req,res) => {
       html: output // html body
     };
   
-    transporter.sendMail(mailOptions, (error, info) => {
+    /*transporter.sendMail(mailOptions, (error, info) => {
       if (error) {
-        res.send("Error: " + error);
-      } else {
-        console.log('Message %s sent: %s', info.messageId, info.response);
-      }
-    });
-    
-    console.log("email sent");
+        return res.status(400).json({ 
+          errors: [{ 
+            msg: "Error sending the email",
+            error: error
+          }] 
+        });
+      } 
+    }); */
+    res.status(200).json({ msg: "Thank you for conctacting me. I'll be in touch soon." });
 });
 
 let port = process.env.PORT;
@@ -56,4 +92,7 @@ if (port == null) {
     port = 3000;
 }
 
-app.listen(port, () => console.log(`Server started on port ${port}`));
+app.listen(port, (err) => {
+  if (err) console.error(err.message);
+  console.log(`Server started on port ${port}`)
+});
